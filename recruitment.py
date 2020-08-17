d4e9e7066acb7659c8ddace9d16929327ae72209
Mohammed Alzaid <m.al-zaid@live.com>
2020-08-17 15:34:36 +0300

    nothing new

--- recruitment.py
+++ recruitment.py
@@ -1,11 +1,13 @@
 def main():
     # write your code here
     cv = {}
+
     print("Welcome to the special recruitment program, please answer the following questions:")
     cv["name"] = input("What's your name? ")
     cv["age"] = int(input("How old are you? "))
     cv["experience"] = int(input("How many years of experience do you have? "))
     cv["skills"] = []
+
     skills = ["Python", "C++", "Javascript", "Meeting", "Leeting", "Eating"]
 
     print("\nSkills:")
@@ -17,6 +19,7 @@ def main():
 
     cv["skills"].append(skills[int(input("Choose a skill from above by entering its number: ")) - 1])
     cv["skills"].append(skills[int(input("Choose another skill from above by entering its number: ")) - 1])
+
     if 25 <= cv["age"] <= 40 and cv["experience"] > 5 and skills[5] in cv["skills"]:
         print("You have been accepted , %s" % cv["name"])
     else:
